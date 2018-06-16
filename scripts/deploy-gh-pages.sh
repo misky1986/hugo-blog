3e24468c341725bc9abac47b581482a24602fee8
misky1986 <mskydt86@gmail.com>
2018-06-16 13:32:10 +0200

    test

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -46,48 +46,9 @@ echo "Git status"
 git status
 
 echo "Add all files"
-git add .
+git add -A
 
 echo "Add all files"
 git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}"
 git push origin gh-pages
 echo "Finished Deployment!"
-#mkdir gh-pages-branch
-#cd gh-pages-branch
-# now lets setup a new repo so we can update the gh-pages branch
-#git init
-#git remote add --fetch origin "$remote"
-
-#echo "$remote"
-
-#echo "Files in folder: "
-
-#pwd
-#ls -al
-
-# switch into the the gh-pages branch
-#if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
-#then
- #   git checkout gh-pages
-    # delete any old site as we are going to replace it
-    # Note: this explodes if there aren't any, so moving it here for now
-    # git rm -rf .
-#else
- #   git checkout --orphan gh-pages
-#fi
-
-# copy over or recompile the new site
-#cp -a "../${siteSource}/." .
-
-# stage any changes and new files
-#git add -A
-# now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
-#git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
-# and push, but send any output to /dev/null to hide anything sensitive
-#git push --force --quiet origin gh-pages
-# go back to where we started and remove the gh-pages git repo we made and used
-# for deployment
-#cd ..
-#rm -rf gh-pages-branch
-
-#echo "Finished Deployment!"
