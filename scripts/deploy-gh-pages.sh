40939bc0a8c0e8a011cbe39125ec1aaef373621f
misky1986 <mskydt86@gmail.com>
2018-06-16 13:22:47 +0200

    Automatic deployment

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -8,48 +8,86 @@
 set -e
 
 # show where we are on the machine
-pwd
-remote=$(git config remote.origin.url)
-
-# make a directory to put the gp-pages branch
-mkdir gh-pages-branch
-cd gh-pages-branch
-# now lets setup a new repo so we can update the gh-pages branch
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
-git init
-git remote add --fetch origin "$remote"
-
-echo "$remote"
-
-echo "Files in folder: "
+remote=$(git config remote.origin.url)
 
 pwd
+# make a directory to put the gp-pages branch
+echo "Current folder"
+pwd
+git clone git@github.com:misky1986/hugo-blog.git
+cd hugo-blog
+
+echo "Current folder"
+pwd
+
+echo "Building hugo site"
+hugo
+echo "Finished building hugo site"
 ls -al
 
+echo "Deleting all other files and folder the public and .git"
+find . -maxdepth 1 ! -name 'public' ! -name '.git' -exec rm -rf {} \;
+ls -al
+
+echo "moving all files and folders within public one level up"
+mv public/* .
+ls -al
+
+echo "Deleting empty public folder"
+rm -rf public 
+ls -al
+
+echo "Checking out gh-pages branch"
+git checkout gh-pages
+
+echo "Git status"
+git status
+
+echo "Add all files"
+git add .
+
+echo "Add all files"
+git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}"
+git push origin gh-pages
+echo "Finished Deployment!"
+#mkdir gh-pages-branch
+#cd gh-pages-branch
+# now lets setup a new repo so we can update the gh-pages branch
+#git init
+#git remote add --fetch origin "$remote"
+
+#echo "$remote"
+
+#echo "Files in folder: "
+
+#pwd
+#ls -al
+
 # switch into the the gh-pages branch
-if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
-then
-    git checkout gh-pages
+#if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
+#then
+ #   git checkout gh-pages
     # delete any old site as we are going to replace it
     # Note: this explodes if there aren't any, so moving it here for now
     # git rm -rf .
-else
-    git checkout --orphan gh-pages
-fi
+#else
+ #   git checkout --orphan gh-pages
+#fi
 
 # copy over or recompile the new site
 #cp -a "../${siteSource}/." .
 
 # stage any changes and new files
-git add -A
+#git add -A
 # now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
-git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
+#git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 # and push, but send any output to /dev/null to hide anything sensitive
-git push --force --quiet origin gh-pages
+#git push --force --quiet origin gh-pages
 # go back to where we started and remove the gh-pages git repo we made and used
 # for deployment
-cd ..
-rm -rf gh-pages-branch
+#cd ..
+#rm -rf gh-pages-branch
 
-echo "Finished Deployment!"
+#echo "Finished Deployment!"
